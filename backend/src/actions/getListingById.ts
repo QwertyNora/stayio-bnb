d4ff338c277cb2f5ba9e6400b57b9031a27bba4a
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-10-25 15:19:48 +0200

    delete comments

--- backend/src/actions/getListingById.ts
+++ backend/src/actions/getListingById.ts
@@ -32,29 +32,3 @@ export async function getListingById(
     return null;
   }
 }
-
-// import { Listing } from "@prisma/client";
-
-// const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
-
-// export async function getListingById(id: string): Promise<Listing | null> {
-//   const url = new URL(`${BASE_URL}/api/listings/${id}`);
-
-//   try {
-//     const response = await fetch(url, {
-//       next: {
-//         tags: [`listing:${id}`],
-//       },
-//     });
-
-//     if (!response.ok) {
-//       throw new Error("Unable to get listing");
-//     }
-
-//     const listing: Listing = await response.json();
-//     return listing;
-//   } catch (error: any) {
-//     console.warn("Error fetching listing by ID (action)", error.message);
-//     return null;
-//   }
-// }
