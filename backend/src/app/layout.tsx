19747f6c517a15ee48a31d1b6c8b55d9b9549dc2
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-11-06 14:21:36 +0100

    add padding on layout

--- backend/src/app/layout.tsx
+++ backend/src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Providers>
           <Navigation />
-          <main className="flex-grow">{children}</main>
+          <main className="flex-grow py-8">{children}</main>
           <Toaster />
         </Providers>
         <Footer />
@@ -43,50 +43,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-// import localFont from "next/font/local";
-// import "./globals.css";
-// import { Navigation } from "@/components/navigation";
-// // import Navigation from "@/components/navigation";
-// import Footer from "@/components/footer";
-// import Providers from "./Providers";
-// import { Toaster } from "@/components/ui/toaster";
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
-// export const metadata: Metadata = {
-//   title: "StayIO",
-//   description: "Find your perfect stay",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body className={inter.className}>
-//         <Providers>
-//           <Navigation />
-//           {children}
-//           <Toaster />
-//         </Providers>
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
