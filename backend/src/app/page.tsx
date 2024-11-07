9d04eb9db378a81e81df121d9bb1502c262ef212
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-11-07 10:22:56 +0100

    add style changes on home page

--- backend/src/app/page.tsx
+++ backend/src/app/page.tsx
@@ -131,11 +131,11 @@ export default function Home() {
 
       {/* Reviews Section */}
       <section className=" py-12">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-semibold text-center mb-8">
+        <div className="container mx-auto px-4 my-9">
+          <h2 className="text-3xl font-semibold text-center mb-16">
             What Our Guests Say
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-32">
             {reviews.map((review: Review) => (
               <ReviewComponent
                 key={review.id}
